452c33b0e801eef97ce0580715a670bd0252067c
Henrique Silva <henrique.p.silva@outlook.com>
2017-06-30 19:39:20 -0300

    cacetadas

---
+++ teste/teste_read.c
@@ -0,0 +1,25 @@
+#include "../include/t2fs.h"
+#include <stdio.h>
+
+int main() {
+	FILE2 rendeu;
+    char buffer[50];
+    printf("\nIniciando teste da funcao read2.\n");
+    printf("Abrindo o arquivo file1...\n");
+
+    if((rendeu = open2("/file1")) < 0) {
+        printf("Retorno menor que zero. btw, handle e %d", rendeu);
+    } else {
+        printf("Aparentemente deu boa no open. btw, handle e %d", rendeu);
+
+        if (read2(rendeu, buffer, 50) > 0) {
+            printf("eu diria que deu boa\n");
+            puts(buffer);
+        } else {
+            printf("deu merda");
+        }
+    }
+    printf("\nFim do teste.\n");
+
+    return 0;
+}
